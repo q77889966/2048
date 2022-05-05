7e10ff4c394fb87c98828ad2b91f93f7f87ac690
Alan_Chen <2304144391@qq.com>
2022-05-05 20:29:01 +0800

    Update game_1.c

--- game_1.c
+++ game_1.c
@@ -14,6 +14,13 @@
 #define	BRIGHT_MAGENTA	FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_BLUE	//13 前景亮洋红
 #define	BRIGHT_YELLOW		FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN	//14 前景亮黄
 #define	BRIGHT_WHITE FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_BLUE //15 前景亮白
+
+void Hidecursor()
+{
+	CONSOLE_CURSOR_INFO CURSOR_INFO = { 1,0 };
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &CURSOR_INFO);
+}
+
 void PrtTitle() {                    //打印2048
 	COORD pos = { 17,2 };
 	COORD pos_1 = { 17,3 };
@@ -79,6 +86,7 @@ char PrtWelcom(int flag) {
 	printf("||                                          ||");
 	SetConsoleCursorPosition(hOut, pos_12);
 	printf("==============================================");
+
 	SetConsoleTextAttribute(hOut, RED);
 	SetConsoleCursorPosition(hOut, pos_13);
 	printf("1.开始游戏     2.按键说明");
@@ -86,6 +94,7 @@ char PrtWelcom(int flag) {
 	printf("3.游戏规则     4.退出-张峻宸");
 	SetConsoleCursorPosition(hOut, final);
 	SetConsoleTextAttribute(hOut, CYAN);
+
 	if (flag == 0) {
 		printf("请选择[1 2 3 4]:[ ]");
 		SetConsoleCursorPosition(hOut, final_1);
@@ -101,18 +110,113 @@ char PrtWelcom(int flag) {
 }
 
 void Keys() {                 //打印按键说明
-	COORD pos_1 = { 20,22 };
+
+	COORD pos_1 = { 19,22 };
+	COORD pos_2 = { 19,24 };
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hOut, BRIGHT_YELLOW);
+	SetConsoleTextAttribute(hOut, BRIGHT_MAGENTA);
+	SetConsoleCursorPosition(hOut, pos_1);
+	printf("（1）↑、↓、←、→方向键进行游戏操作！");
+	SetConsoleCursorPosition(hOut, pos_2);
+	printf("（2）ESC键退出游戏");
+	Hidecursor();
+	fflush(stdin);
+	getch();
+	system("cls");
+	main();
 }
+
+
 
 void Rules() {                //打印游戏规则
 	system("cls");
-	printf("111");
+	//printf("111");
+
+	COORD pos_1 = { 27,3 };//游戏规则
+
+	COORD pos_2 = { 12,5 };//画框
+	COORD pos_3 = { 12,6 };
+	COORD pos_4 = { 12,7 };
+	COORD pos_5 = { 12,8 };
+	COORD pos_6 = { 12,9 };
+	COORD pos_7 = { 12,10 };
+	COORD pos_8 = { 12,11 };
+	COORD pos_9 = { 12,12 };
+	COORD pos_10 = { 12,13 };
+	COORD pos_11 = { 12,14 };
+	COORD pos_12 = { 12,15 };
+	COORD pos_13 = { 12,16 };
+	COORD pos_14 = { 12,17 };
+
+	COORD pos_15 = { 15,6 };//规则文字
+	COORD pos_16 = { 15,8 };
+	COORD pos_17 = { 15,10 };
+	COORD pos_18 = { 15,12 };
+	COORD pos_19 = { 15,14 };
+	COORD pos_20 = { 15,16 };
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hOut, BRIGHT_MAGENTA);
+	SetConsoleCursorPosition(hOut, pos_1);
+	printf("游戏规则");
+	SetConsoleCursorPosition(hOut, pos_2);
+	SetConsoleTextAttribute(hOut, GREEN);
+	printf("========================================================");
+	SetConsoleCursorPosition(hOut, pos_3);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_4);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_5);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_6);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_7);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_8);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_9);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_10);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_11);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_12);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_13);
+	printf("||                                                    ||");
+	SetConsoleCursorPosition(hOut, pos_14);
+	printf("========================================================");
+
+	SetConsoleCursorPosition(hOut, pos_15);
+	SetConsoleTextAttribute(hOut, CYAN);
+	printf("tip1: 玩家可以通过↑ ↓ ← →方向键来移动方块");
+
+	SetConsoleCursorPosition(hOut, pos_16);
+	SetConsoleTextAttribute(hOut, GREEN);
+	printf("tip2: 按ESC退出游戏");
+
+	SetConsoleCursorPosition(hOut, pos_17);
+	SetConsoleTextAttribute(hOut, BRIGHT_YELLOW);
+	printf("tip3: 玩家选择的方向上,若有相同的数字则合并");
+
+	SetConsoleCursorPosition(hOut, pos_18);
+	SetConsoleTextAttribute(hOut, BRIGHT_CYAN);
+	printf("tip4: 每移动一步，空位随机出现一个2或4");
+
+	SetConsoleCursorPosition(hOut, pos_19);
+	SetConsoleTextAttribute(hOut, RED);
+	printf("tip5: 棋盘被数字填满，无法进行有效移动，游戏失败");
+
+	SetConsoleCursorPosition(hOut, pos_20);
+	SetConsoleTextAttribute(hOut, YELLOW);
+	printf("tip6: 棋盘上出现2048，游戏胜利");
+
+
+	Hidecursor();
+	getch();
 }
 
 void Game_Start() {
-	printf("111");
+	//printf("111");
 }
 
 int main() {
@@ -133,7 +237,7 @@ int main() {
 			Rules();
 		else {
 			flag = 1;
-			PrtWelcom(flag);
+
 		}
 	}
 	return 0;
